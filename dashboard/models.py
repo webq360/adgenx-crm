2f798295a5b594d4fd112f31a906a63057ab526e
mohammad-jowel <mohammadjowel313@gmail.com>
2025-08-27 15:51:27 +0600

    number feild added

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True)
     pass
 
 class DepositTransaction(models.Model):
--- dashboard/views.py
+++ dashboard/views.py
@@ -86,6 +86,7 @@ def auth(request):
             first_name = request.POST.get('first_name')
             last_name = request.POST.get('last_name')
             email = request.POST.get('email')
+            phone_number = request.POST.get('phone_number')
             password = request.POST.get('password')
             password2 = request.POST.get('password2')
 
@@ -100,6 +101,7 @@ def auth(request):
                         password=password, 
                         first_name=first_name, 
                         last_name=last_name,
+                        phone_number=phone_number,
                         is_active=False
                     )
                     login(request, user)
