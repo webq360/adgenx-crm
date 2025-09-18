2958d16e18f9bd9a0b017ca4c2a36ca52dad85a9
mohammad-jowel <mohammadjowel313@gmail.com>
2025-09-18 20:46:02 +0600

    is_verified field added to user model

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True)
+    is_verified = models.BooleanField(default=False)
     pass
 
 class DepositTransaction(models.Model):
